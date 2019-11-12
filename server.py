1a38404f8542923bf540d804466ab9ad9fb7a0c9
ZiyiLiao <eziyi1996@icloud.com>
2019-11-12 14:48:03 -0500

    Update

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
+import psycopg2
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -34,23 +35,20 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #
 #     DATABASEURI = "postgresql://biliris:foobar@104.196.18.7/w4111"
 #
-DATABASEURI = "postgresql://user:password@104.196.18.7/w4111"
+DATABASEURI = "postgresql://nz2297:0875@34.74.165.156/proj1part2"
 
-
-#
 # This line creates a database engine that knows how to connect to the URI above.
 #
 engine = create_engine(DATABASEURI)
 
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+
+# engine.execute("""CREATE TABLE IF NOT EXISTS test (
+#   id integer,
+#   name varchar
+# );""")
+# engine.execute("""INSERT INTO test(id, name) VALUES (1,'grace hopper'), (2,'alan turing'), (3,'ada lovelace');""")
+# engine.execute("DROP TABLE test")
+
 
 
 @app.before_request
@@ -65,7 +63,7 @@ def before_request():
   try:
     g.conn = engine.connect()
   except:
-    print "uh oh, problem connecting to database"
+    print("uh oh, problem connecting to database")
     import traceback; traceback.print_exc()
     g.conn = None
 
@@ -107,19 +105,10 @@ def index():
   """
 
   # DEBUG: this is debugging code to see what request looks like
-  print request.args
+  # print(request.args)
 
 
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
 
-  #
   # Flask uses Jinja templates, which is an extension to HTML where you can
   # pass data to a template and dynamically generate HTML based on the data
   # (you can think of it as simple PHP)
@@ -145,40 +134,276 @@ def index():
   #     <div>{{n}}</div>
   #     {% endfor %}
   #
-  context = dict(data = names)
+  # context = dict(data = names)
 
 
   #
   # render_template looks in the templates/ folder for files.
   # for example, the below file reads template/index.html
   #
-  return render_template("index.html", **context)
+  # return render_template("index.html", **context)
 
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("another.html")
+  return render_template("index.html")
+
+@app.route('/borough', methods = ['GET'])
+def borough():
+  cursor = g.conn.execute("SELECT b.bname, b.area, b.population, c.type, COUNT(*) AS counts FROM borough b JOIN crime c ON c.bid = b.bid GROUP BY c.type, b.bname, b.area, b.population")
+  borough = {}
+  for result in cursor:
+    key = (result['bname'], result['area'], result['population'])
+    info = (result['type'], result['counts'])
+    if key not in borough:
+      borough[key] = [info]
+    else:
+      borough[key].append(info)
+  cursor.close()
+  return render_template('borough.html', data = borough)
+
+@app.route('/search', methods = ['GET', 'POST'])
+def search():
+  bid = request.args.get("bid")
+  level = request.args.get("fund")
+  score = request.args.get("score")
+  cursor = g.conn.execute("SELECT bname,bid FROM borough")
+  names = []
+  for result in cursor:
+    temp = (result['bname'], result['bid'])
+    names.append(temp)
+  cursor.close()
+
+  step1 = g.conn.execute("SELECT DISTINCT level FROM fund")
+  fund_result = []
+  for result in step1:
+    fund_result.append(result['level'])
+  step1.close()
+
+  step2 = g.conn.execute("SELECT DISTINCT level FROM sat")
+  score_result = []
+  for result in step2:
+    score_result.append(result['level'])
+  step2.close()
+
+  if (bid == None and level == None and score == None):
+    return render_template("search.html", names = names, fund = fund_result, score = score_result)
+  elif (bid != None and level == None and score == None):
+    c = g.conn.execute("SELECT distinct initcap(sname), s.sid FROM school s, fund f, sat where s.bid = %s and f.sid = s.sid and sat.sid = s.sid", bid)
+  elif (level != None and score == None and bid == None):
+    c = g.conn.execute("SELECT distinct initcap(sname), s.sid FROM school s, fund f, sat where f.level = %s and f.sid = s.sid and sat.sid = s.sid",level)
+  elif(level == None and bid == None and score != None): 
+    c = g.conn.execute("SELECT distinct initcap(sname), s.sid FROM school s, fund f, sat where f.sid = s.sid and sat.sid = s.sid and sat.level = %s",score)
+  elif (score == None):
+    c = g.conn.execute("SELECT distinct initcap(sname), s.sid FROM school s, fund f, sat where s.bid = %s and f.level = %s and f.sid = s.sid and sat.sid = s.sid", bid, level)
+  elif (level == None):
+    c = g.conn.execute("SELECT distinct initcap(sname), s.sid FROM school s, fund f, sat where s.bid = %s and f.sid = s.sid and sat.sid = s.sid and sat.level = %s", bid, score)
+  elif (bid == None):
+    c = g.conn.execute("SELECT distinct initcap(sname), s.sid FROM school s, fund f, sat where f.level = %s and f.sid = s.sid and sat.sid = s.sid and sat.level = %s",level,score)
+  elif (bid != None and level != None and score != None):
+    c = g.conn.execute("SELECT distinct initcap(sname), s.sid FROM school s, fund f, sat where s.bid = %s and f.level = %s and f.sid = s.sid and sat.sid = s.sid and sat.level = %s", bid, level,score)
+  schools = c.fetchall()
+  c.close()
+  return render_template("search.html", names = names, result = schools, fund = fund_result, score = score_result)
+  
+
+
+@app.route('/school_search')
+def school_search():
+  cursor = g.conn.execute("SELECT sname,sid FROM school")
+  names = []
+  for result in cursor:
+    temp = (result['sname'], result['sid'])
+    names.append(temp)
+  cursor.close()
+  return render_template("school_search.html", names = names)
+
+
+@app.route('/school_info', methods=['GET'])
+def school_info():
+  fsid = request.args.get("sid") # extract selected sid
+  cursor = g.conn.execute("SELECT * FROM school s, borough b WHERE s.sid = %s and s.bid = b.bid", fsid)
+  rows = cursor.fetchall()
+  cursor.close() 
+
+  c = g.conn.execute("SELECT * FROM survey s WHERE s.sid = %s", fsid)
+  survey = c.fetchall()
+  c.close() 
+
+  s = g.conn.execute("SELECT * FROM sat WHERE sat.sid = %s", fsid)
+  sat = s.fetchall()
+  s.close()
+
+  f = g.conn.execute("SELECT * FROM fund WHERE fund.sid = %s", fsid)
+  fund = f.fetchall()
+  f.close()
+  return render_template('school_info.html', school_id = rows, survey = survey, sat = sat, fund = fund)
+
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+  return render_template("login.html")
+
+
+@app.route('/manage', methods = ['GET','POST'])
+def manage():  
+  userid = request.args.get("userid")
+  pwd = request.args.get("password")
+  try:
+    digit = int(userid[0])
+    sid = userid
+    cursor = g.conn.execute("SELECT * FROM school where sid = %s and password = %s", sid, pwd)
+    s = cursor.fetchall()
+    cursor.close()
+    if len(s) == 0:
+      return render_template("login.html", err = sid)
+    else:
+      c1 = g.conn.execute("SELECT distinct course_id, course_name, tname FROM teach t, teacher t1 where t.sid = %s and t.tid = t1.tid order by course_id", sid)
+      teach = c1.fetchall()
+      c1.close()
+
+      c2 = g.conn.execute("SELECT * from student where left(stuid,6) like %s order by stuid;", sid)
+      students = c2.fetchall()
+      c2.close()
+
+      c3 = g.conn.execute("SELECT * from teacher where left(tid,6) like %s order by tid;", sid)
+      teacher = c3.fetchall()
+      c3.close()
+      
+      c4 = g.conn.execute("SELECT * FROM survey s WHERE s.sid = %s", sid)
+      survey = c4.fetchall()
+      c4.close()   
+
+      sat = g.conn.execute("SELECT * FROM sat WHERE sat.sid = %s order by year", sid)
+      score = sat.fetchall()
+      sat.close()
+
+      f = g.conn.execute("SELECT * FROM fund WHERE fund.sid = %s", sid)
+      fund = f.fetchall()
+      f.close()
+
+      return render_template("manage.html", data = s, sid = sid, teach = teach, survey = survey, fund = fund, sat = score, student = students, teacher = teacher)
+  except:
+    bname = userid
+    print("bname :" + bname)
+    cursor = g.conn.execute("SELECT * FROM borough where bname = %s and password = %s", bname, pwd)
+    s = cursor.fetchall()
+    cursor.close()
+    if len(s) == 0:
+      return render_template("login.html", err = bname)
+    else:
+      c1 = g.conn.execute("SELECT fid, sname, f.year, amount FROM borough b, fund f, school s where b.bname = %s and b.bid = f.bid and f.sid = s.sid", bname)
+      fund = c1.fetchall()
+      c1.close()
+
+      c2 = g.conn.execute("SELECT sname, sid FROM school s, borough b where b.bname = %s and b.bid = s.bid", bname)
+      names = c2.fetchall()
+      c2.close()
+      return render_template("manage.html", data = s, bname = bname , names = names, fund = fund)
+
+  
+
+@app.route('/insert_borough', methods=['GET',"POST"])
+def insert_borough():
+  bname = request.args.get("bname")
+  cursor = g.conn.execute("SELECT bid, password FROM borough where bname = %s", bname)
+  s = cursor.fetchone()
+  bid = s[0]
+  pwd = s[1]
+  cursor.close() 
+  item = request.args.get("item")
+
+  if request.method == "POST":
+    if item == 'fund':
+      fid = request.form['fid']
+      sid = request.form['sid']
+      year = request.form['year']
+      amount = request.form['amount']
+      c1 = g.conn.execute("INSERT INTO fund(fid, sid, bid, year, amount) VALUES (%s, %s, %s, %s, %s)", (fid, sid, bid, year,amount))
+      c2 = g.conn.execute("UPDATE fund set level = case when amount >82000 then 'High' when amount >75000 and amount < 82000 then 'Medium' else 'low' end")
+      c1.close()
+      c2.close()
+    elif item == 'school':
+      sid = request.form['sid']
+      sname = request.form['sname']
+      enrollment = request.form['enrollment']
+      principal = request.form['principal']
+      password = request.form['password']
+      c1 = g.conn.execute("INSERT INTO school(sid, sname,enrollment, bid, principal, password) VALUES (%s,%s,%s,%s,%s,%s)", (sid,sname,enrollment,bid,principal,password))
+      c1.close()
+    return render_template("insert_borough.html", bname = bname, pwd = pwd, item = item, message = "*  Finished!")
+  return render_template("insert_borough.html", bname = bname, pwd = pwd,item = item)
+
+
+@app.route('/insert_school', methods=['GET',"POST"])
+def insert_school():
+  sid = request.args.get("sid")
+  cursor = g.conn.execute("SELECT password FROM school where sid = %s", sid)
+  s = cursor.fetchone()
+  pwd = s[0]
+  cursor.close()
+  item = request.args.get("item")
+
+  if request.method == "POST":
+    if item == 'sat':
+      year= request.form['year']
+      reading = request.form['reading']
+      mathematics = request.form['mathematics']
+      writing = request.form['writing']
+      total = int(reading) + int(mathematics) + int(writing)
+      c1 = g.conn.execute("INSERT INTO sat(sid, year, reading, mathematics, writing) VALUES (%s, %s, %s, %s, %s)", (sid, year, reading, mathematics,writing))
+      c2 = g.conn.execute("UPDATE sat set total = %s where sid = %s and year = %s", (total, sid, year))
+      c3 = g.conn.execute("UPDATE sat set level = case when total >1200 then 'High' when total >1125 and total < 1200 then 'Medium' else 'low' end")
+      c1.close()
+      c2.close()
+      c3.close()
+    elif item == 'student':
+      temp= request.form['stuid']
+      name = request.form['name']
+      stuid = sid +temp
+      c1 = g.conn.execute("INSERT INTO student(stuid, name) VALUES (%s, %s)", (stuid, name))
+      c1.close()
+    elif item == 'teacher':
+      course_id= request.form['cid']
+      course_name = request.form['cname']
+      tid = request.form['tid']
+      stuid= request.form['stuid']
+      c1 = g.conn.execute("INSERT INTO teach(course_id, course_name, tid, stuid) VALUES (%s, %s, %s, %s)", (course_id, course_name, tid, stuid))
+      c1.close()
+    elif item == 'course':
+      tid = sid + request.form['tid']
+      name = request.form['cname']
+      stuid = sid + request.form['stuid']
+      course_id = request.form['cid']
+      c1 = g.conn.execute("INSERT INTO teach(course_id, course_name, tid, stuid, sid) VALUES (%s, %s, %s, %s, %s)", (course_id, name, tid, stuid, sid))
+      c1.close()
+
+    return render_template("insert_school.html", sid = sid , item = item, message = "* Finished!", pwd = pwd)
+  else:
+    return render_template("insert_school.html",sid = sid,item = item, pwd = pwd)
+
+
+
+
+# @app.route('/check', methods = ['GET', 'POST'])
+# def check():
+#   c2 = g.conn.execute("SELECT * FROM sat where sid = '01M292'")
+#   teach = c2.fetchall()
+#   c2.close()
+  
+#   return render_template("check.html", names = teach)
 
 
 # Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  g.conn.execute('INSERT INTO test VALUES (NULL, ?)', name)
-  return redirect('/')
+# @app.route('/add', methods=['GET','POST'])
+# def add():
+#   if request.method == "POST":
+#     name = request.form['name']
+#     g.conn.execute("INSERT INTO test(name) VALUES (%s)", (name))
+#     return render_template("add.html",res = "y")
+#   return render_template("add.html")
 
 
-@app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
+
+
+  
 
 
 if __name__ == "__main__":
@@ -187,8 +412,10 @@ if __name__ == "__main__":
   @click.command()
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
+  #@click.argument('HOST', default='0.0.0.0')
+  @click.argument('HOST', default='35.237.146.64')
   @click.argument('PORT', default=8111, type=int)
+  #@click.argument('PORT', default=8080, type=int)
   def run(debug, threaded, host, port):
     """
     This function handles command line parameters.
@@ -203,7 +430,7 @@ if __name__ == "__main__":
     """
 
     HOST, PORT = host, port
-    print "running on %s:%d" % (HOST, PORT)
+    print("running on %s:%d" % (HOST, PORT))
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 
